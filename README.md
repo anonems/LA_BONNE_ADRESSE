9f87ede68d755688b5abd111316253567cc15901
anonems <93661372+anonems@users.noreply.github.com>
2022-06-21 14:19:44 +0200

    Co-authored-by: SenkaX <SenkaX@users.noreply.github.com> Co-authored-by: Hugo Fesniere <HugoF18@users.noreply.github.com> Co-authored-by: yumeei <yumeei@users.noreply.github.com> Co-authored-by: carim75 <carim75@users.noreply.github.com> Co-authored-by: UnMaxDeJoie <UnMaxDeJoie@users.noreply.github.com> Co-authored-by: GRANIER Hugo <hugogranier@users.noreply.github.com> Co-authored-by: Arfroxys <Arfroxys@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LA_BONNE_ADRESSE
-Projet de fin de première Année au bachelor développement web, plateforme (PWA) qui permet au petits commerçants sur Paris de gagné en visibilité sur le web et de faire évoluer leurs clientèles.
+Projet de fin de première année au bachelor développement web, plateforme (PWA) qui permet aux petits commerçants sur Paris de gagner en visibilité sur le web et de faire évoluer leur clientèle.
+
---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CALENDRIER</title>
+    <link rel="shortcut icon" href="data/lba/logo.png" type="image/png">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="manifest" href="manifest.webmanifest"> -->
+    <!-- <script src="index.js" defer></script> -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+</head>
+
+<header class="headbar">
+    <button class="ubtn" class="add-button"><span class="material-symbols-outlined">download</span></button>
+    <img src="data/lba/logo.png" width="100px" alt="logo">
+    <button class="dnmode" onclick="myFunction()"><span id="dark"
+            class="material-symbols-outlined">dark_mode</span></button>
+    <script>
+        let element = document.body;
+        if (document.cookie.split(';')[0].split("=")[1] == "dark_mode") {
+            document.getElementById('dark').innerHTML = 'dark_mode';
+            element.classList.add('dark-mode');
+        } else if (document.cookie.split(';')[0].split("=")[1] == "light_mode") {
+            document.getElementById('dark').innerHTML = 'light_mode';
+            element.classList.remove('dark-mode');
+        }
+
+        function myFunction() {
+            if (document.getElementById('dark').innerText == 'light_mode') {
+                document.cookie = "cmode = dark_mode";
+                document.getElementById('dark').innerHTML = 'dark_mode';
+                element.classList.add('dark-mode');
+            } else if (document.getElementById('dark').innerText == 'dark_mode') {
+                document.cookie = "cmode = light_mode";
+                document.getElementById('dark').innerHTML = 'light_mode';
+                element.classList.remove('dark-mode');
+            }
+        }
+    </script>
+</header>
+
+<body>
+    <!-- FEED -->
+    <section class="today" id="stoday">
+        PAGE 1
+    </section>
+
+    <!-- RECHERCHE -->
+    <section class="schedule" id="sschedule">
+        PAGE 2
+    </section>
+
+    <!-- PROFIL -->
+    <section class="setting" id="ssetting">
+        PAGE 3
+    </section>
+
+</body>
+
+<footer>
+    <nav class="navbar">
+        <button id="today" class="nbuton"><span class="material-symbols-outlined">home</span> <span
+                id="hdt">Feed</span></button>
+        <button id="schedule" class="nbuton"><span class="material-symbols-outlined">search</span> <span
+                id="hdt">Recherche</span></button>
+        <button id="setting" class="nbuton"><span class="material-symbols-outlined">person</span> <span
+                id="hdt">Profil</span></button>
+    </nav>
+    <script>
+        let bt1 = document.getElementById("today");
+        let bt2 = document.getElementById("schedule");
+        let bt3 = document.getElementById("setting");
+        let s1 = document.getElementById("stoday");
+        let s2 = document.getElementById("sschedule");
+        let s3 = document.getElementById("ssetting");
+        bt1.style.boxShadow = "0px -5px 0px 0px orange"
+        bt1.addEventListener("click", () => {
+            s1.style.display = "block";
+            bt1.style.boxShadow = "0px -5px 0px 0px orange"
+            bt2.style.boxShadow = "0px -5px 0px 0px var(--colortwo)"
+            bt3.style.boxShadow = "0px -5px 0px 0px var(--colortwo)"
+            s2.style.display = "none";
+            s3.style.display = "none";
+            s1.style.animation = "moveToRight 0.5s ease-in-out"
+        })
+        bt2.addEventListener("click", () => {
+            s2.style.display = "block";
+            bt2.style.boxShadow = "0px -5px 0px 0px orange"
+            bt3.style.boxShadow = "0px -5px 0px 0px var(--colortwo)"
+            bt1.style.boxShadow = "0px -5px 0px 0px var(--colortwo)"
+            s1.style.display = "none";
+            s3.style.display = "none";
+            s2.style.animation = "moveToRight 0.5s ease-in-out"
+
+        })
+        bt3.addEventListener("click", () => {
+            s3.style.display = "block";
+            bt3.style.boxShadow = "0px -5px 0px 0px orange"
+            bt2.style.boxShadow = "0px -5px 0px 0px var(--colortwo)"
+            bt1.style.boxShadow = "0px -5px 0px 0px var(--colortwo)"
+            s2.style.display = "none";
+            s1.style.display = "none";
+            s3.style.animation = "moveToRight 0.5s ease-in-out"
+        })
+    </script>
+</footer>
+<!-- <script>
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js')
+            .then((registration) => {
+                console.log('Service Worker: Registered', registration);
+            });
+        }
+    </script> -->
+
+</html>
